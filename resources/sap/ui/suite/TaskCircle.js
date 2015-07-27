92f1193fe3a2ec28bed023f03b077bb737bc3d34
OpenUI5 <openui5@sap.com>
2015-07-27 10:49:47 +0200

    1.30.3

--- resources/sap/ui/suite/TaskCircle.js
+++ resources/sap/ui/suite/TaskCircle.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/EnabledP
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author Svetozar Buzdumovic
-	 * @version 1.30.2
+	 * @version 1.30.3
 	 *
 	 * @constructor
 	 * @public
