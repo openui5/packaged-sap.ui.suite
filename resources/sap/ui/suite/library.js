d5782d64118a3a5f413698a2343a8fd363be73ed
OpenUI5 <openui5@sap.com>
2017-01-19 08:49:16 +0100

    1.28.43

--- resources/sap/ui/suite/library.js
+++ resources/sap/ui/suite/library.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2016 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2017 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -19,14 +19,14 @@ sap.ui.define(['jquery.sap.global',
 	 * @namespace
 	 * @name sap.ui.suite
 	 * @author SAP SE
-	 * @version 1.28.42
+	 * @version 1.28.43
 	 * @public
 	 */
 
 	// delegate further initialization of this library to the Core
 	sap.ui.getCore().initLibrary({
 		name : "sap.ui.suite",
-		version: "1.28.42",
+		version: "1.28.43",
 		dependencies : ["sap.ui.core"],
 		types: [
 			"sap.ui.suite.TaskCircleColor"
@@ -43,7 +43,7 @@ sap.ui.define(['jquery.sap.global',
 	/**
 	 * Defined color values for the Task Circle Control
 	 *
-	 * @version 1.28.42
+	 * @version 1.28.43
 	 * @enum {string}
 	 * @public
 	 * @ui5-metamodel This enumeration also will be described in the UI5 (legacy) designtime metamodel
