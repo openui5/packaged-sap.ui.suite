9154e0eadefc45a832707fe142f2138d6d66c91e
OpenUI5 <openui5@sap.com>
2017-07-24 16:39:30 +0200

    1.46.12

--- resources/sap/ui/suite/VerticalProgressIndicator.js
+++ resources/sap/ui/suite/VerticalProgressIndicator.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/EnabledP
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author Svetozar Buzdumovic
-	 * @version 1.46.11
+	 * @version 1.46.12
 	 *
 	 * @constructor
 	 * @public
